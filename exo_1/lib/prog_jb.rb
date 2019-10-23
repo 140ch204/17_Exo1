1f74c4339627b9824ca5d5cb1d54e335a5a2c6e4
140ch204 <140ch204_github@gadz.org>
2019-10-23 23:27:05 +0200

    V1

--- exo_1/lib/prog_jb.rb
+++ exo_1/lib/prog_jb.rb
@@ -31,4 +31,26 @@ require 'faker'
 
   gem faker
 
-  
+  gem table_print
+
+
+  rails g model User first_name:string last_name:string age:integer city:string
+
+# Ajouter un modèle : 
+  class CreateArticles < ActiveRecord::Migration[5.2]
+    def change
+      create_table :articles do |t|
+        t.string :title
+        t.string :subject
+        t.text :content
+        t.belongs_to :user, index: true #cette ligne rajoute la référence à la table users
+        t.references :user, foreign_key: true, index: true #cette ligne rajoute la référence à la table users
+        t.timestamps
+      end
+    end
+  end
+
+# Ajouter une colonne référence
+  def change
+    add_reference :articles, :user, foreign_key: true
+  end
